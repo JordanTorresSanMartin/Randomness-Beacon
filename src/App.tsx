471829cf30d880c43ecb75dc3029be3f67dc2e2e
Jordan <jordan@soymomo.com>
2021-12-09 01:56:11 -0300

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, Fragment} from 'react';
+import {OutputValue} from './interfaces';
 import './App.css';
+import axios, {AxiosResponse} from 'axios';
 
 function App() {
+  const [outputValueData, setOutputValueData] = useState<OutputValue[]>([]);
+  const [clickedButton, setClickedButton] = useState('');
+
+  const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const button: HTMLButtonElement = event.currentTarget;
+    setClickedButton(outputValueData.toString());
+  };
+
+
+  useEffect(() => {
+    axios
+      .get("https://beacon.nist.gov/beacon/2.0/chain/last/pulse/last")
+      .then((response: AxiosResponse) =>{
+        setOutputValueData(response.data.pulse.outputValue);
+      })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">
+            <img src="https://evoting.com/assets/images/evoting_logo.svg" className="App-logo" alt="logo" />
+            <button className="App-button" onClick={buttonHandler}>Mostrar último valor</button>
+            <a className="App-a">
+            {clickedButton !== ""
+              ? `"${clickedButton}"` 
+              : ""}
+            </a>
+        </header>
+    </div>
     </div>
   );
 }
+
+const OutputValueList: React.FC<OutputValue> = ({outputValue}) => {
+  return (
+      <Fragment>
+          <ul>
+              <li>
+                <span>{outputValue}</span>
+              </li>
+          </ul>
+      </Fragment>
+  );
+};
 
 export default App;
